8308845eaf34abf13cdbe131ad3b080c6942f4e8
piekarzmike <mikepiekarz@hotmail.com>
2020-07-23 19:34:35 -0500

    Exercise#4

--- Box.java
+++ Box.java
@@ -16,42 +16,44 @@
 	//	prints out to the screen a set of * (star) characters with size rows and size columns. Enter code
 	//	in the main method to create a Box object, change its size to 5 (using the dot operator), and
 	//	then invoke its printBox method. Run the program and fix any errors you may have.
-	//
 	//-----------------------------------------------------------------------------------------
 	
 public class Box {
-	private static int size = 5;
-
+	private static int size = 0;
+	
 	public static void main(String[] args) {
-		// Declare and instantiate a object reference variable boxDrawer
-		Box boxDrawer = new Box();
+		// Declare and instantiate a object reference variable called Box1
+		Box Box1 = new Box();
+		
+		// 
+		Box1.size = 5;
 
-		// Call printbox method
-		boxDrawer.printbox(size);
+		// Call printBox method
+		Box1.printBox();
+
 	}
-
+	
 
 	//-----------------------------------------------------------------------------------------
-	// Create printbox method
+	// Create printBox method
 	//-----------------------------------------------------------------------------------------
-	public void printbox(int size) {
+	public static void printBox() {
+		// Declare and initialize the count and rowCount variable
+		int colCount = 0 ;
+		int rowCount = size;
 
-		// Declare and initialize the count and rCount variable
-		int count = 0 ;
-		int rCount = size;
-
-		//  Print * in same line the number of time per the size variable
-		while (rCount != 0) {
-			while (count<size) {
-				System.out.print("*");
-				count++;
+		//  Print * out in a patter in accordance with it's size
+		while (rowCount != 0) {
+			while (colCount < size) {
+				System.out.print("*"); //  Print * in same line per the number of times stated in the size variable
+				colCount++;
 			}
-			// Set print area to a new line and perform this till number of lines equal the size variable
+			// Set print area to next line
 			System.out.println("");
-			rCount--;
-			count=0;
+			rowCount--;
+			colCount=0; // Resets count variable to zero
 		}
-
+		
 	}
 
 }
